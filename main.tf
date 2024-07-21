be7859a242683ea43086fcc352a3d6cd27859210
ninjaspeed55 <cluicanada@outlook.com>
2024-07-21 16:38:07 +0000

    fix format

--- main.tf
+++ main.tf
@@ -24,8 +24,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "securestorage" {
-  source  = "app.terraform.io/TestOrg11135/securestorage/azurerm"
-  version = "1.0.0"
+  source               = "app.terraform.io/TestOrg11135/securestorage/azurerm"
+  version              = "1.0.0"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
   storage_account_name = "chickenstorage"
