886790e301cbf844f7106433307e9e50be4b4b19
ninjaspeed55 <cluicanada@outlook.com>
2024-07-21 16:36:30 +0000

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+  cloud {
+    organization = "TestOrg11135"
+
+    workspaces {
+      name = "Hands-On_With_Terraform_On_Azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "813-ebcf5737-hands-on-with-terraform-on-azure"
+  location = "West US"
+}
+
+module "securestorage" {
+  source  = "app.terraform.io/TestOrg11135/securestorage/azurerm"
+  version = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "chickenstorage"
+}
+
